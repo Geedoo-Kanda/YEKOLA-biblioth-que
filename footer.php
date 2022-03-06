6b17c521ad432f83f6047babb056e11f8c23fa42
Geedoo Kanda <geedookanda06@gmail.com>
2022-03-06 08:44:53 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,44 @@
+<footer class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <h4>Qui sommes nous</h4>
+                    <p class="nous">Une plateforme des des etudiants de première licence de l'université de kinshasa en République
+                        Démocratique du Congo qui vous permet de consulter et télécharger les ouvrages des auteurs
+                        reconnus</p>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <h4>Aboonnez vous</h4>
+                    <a href="login.php" type="button" class="btn btn-outline-success nav-link">Se connecter</a>
+                    <a href="logup.php" type="button" class="btn btn-primary nav-link">S'inscrire</a>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+                    <h4>Coordonnées</h4>
+                    <div>
+                        <span class="fa fa-phone"></span>
+                        <a href="tel:+243 972 062 655">+243 972 062 655</a>
+                    </div>
+                    <div>
+                        <span class="fa fa-envelope"></span>
+                        <a href="mailto:geedookanda06@gmail.com">geedookanda06@gmail.com</a>
+                    </div>
+                    <div>
+                        <span class="fa fa-map-marker-alt"></span>
+                        245, av. Noiki C/ Gombe Q/ Gare centrale, Ref: non loin de Bluesat
+                    </div>
+                </div>
+                <div class="copyr col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <p>&copy; Copyright <strong>Groupe 88 conception</strong>. All Rights Reserved</p>
+                    <p>Designed by <a href="https://github.com/Geedoo-Kanda">KANDA GEEDOO</a></p>
+                </div>
+            </div>
+        </footer>
+        <a href="#" class="retour-btn"><span class="fa fa-chevron-up"></span></a>
+    </div>
+    <script src="jquery/jquery.min.js"></script>
+    <script src="jquery/jquery-ui.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="aos-master/dist/aos.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
